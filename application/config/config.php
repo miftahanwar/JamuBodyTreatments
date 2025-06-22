bf58585f809a935c1cf962e33ea1450125ec4967
miftahanwar <miftahanwar2442@gmail.com>
2025-06-22 17:18:13 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://miftah-anwar.ct.ws/';
+$config['base_url'] = 'http://localhost/JamuBodyTreatments';
 
 /*
 |--------------------------------------------------------------------------
